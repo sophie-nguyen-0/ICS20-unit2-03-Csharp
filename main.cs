825587b9bdb91f4372c337b420c49e6f32763940
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-13 18:12:21 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: sophie
+// Created on: oct 2020
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        int streetNumber;
+        string streetName;
+      
+        Console.WriteLine("This program gets a user's address");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine("Your street address is: " +  streetNumber + " " +streetName + ".");
+        Console.WriteLine("run while you still can.");
+
+        Console.WriteLine("\nDone.");
+    }
+}
